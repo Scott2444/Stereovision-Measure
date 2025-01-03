168086d3e42ce8b09a0d1bb24be4a70758e1c422
Scott Haakenson <scotty.haakenson@gmail.com>
2025-01-02 23:28:31 -0500

    Computer vision installed and test demo

---
+++ Computer Vision/InstallVerification.py
@@ -0,0 +1,27 @@
+from mmdet.apis import init_detector, inference_detector
+from mmengine.visualization import Visualizer
+import mmcv
+
+image_path = 'demo/demo.jpg'
+
+# Init and run the model
+config_file = 'rtmdet_tiny_8xb32-300e_coco.py'
+checkpoint_file = 'rtmdet_tiny_8xb32-300e_coco_20220902_112414-78e30dcc.pth'
+model = init_detector(config_file, checkpoint_file, device='cpu')  # or device='cuda:0'
+result = inference_detector(model, image_path)
+
+
+# Print the results
+# print(result)
+predictions = result.pred_instances
+print(predictions)
+print(predictions.bboxes.size())
+print(predictions.scores.size())
+
+# Display the results on the original image
+image = mmcv.imread(image_path,
+                    channel_order='rgb')
+visualizer = Visualizer(image=image)
+# single bbox formatted as [xyxy]
+visualizer.draw_bboxes(predictions.bboxes[:3])
+visualizer.show()
---
+++ Computer Vision/README.md
@@ -0,0 +1,9 @@
+# Computer Vision
+
+This project uses MMLab's object detection model which can be found here:
+https://github.com/open-mmlab/mmdetection
+
+## Installation
+
+Follow the steps on this link:
+https://mmdetection.readthedocs.io/en/latest/get_started.html
